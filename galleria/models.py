190bab0bde4dd1d25784ee2458e6ca21276d3120
zizoubrown <zizumoha2015@gmail.com>
2020-10-12 01:55:08 +0300

    modify models.py to correct errors

--- galleria/models.py
+++ galleria/models.py
@@ -17,8 +17,8 @@ class Image(models.Model):
     #image = models.ImageField()
     image_name = models.CharField(max_length=30)
     image_description = models.CharField(max_length=30)
-    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    image_category = models.ForeignKey(Location, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Location, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.image_name
