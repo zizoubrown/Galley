1e2f4b9baff025e7d92764a0e21f14f2cfe57013
zizoubrown <zizumoha2015@gmail.com>
2020-10-12 01:35:53 +0300

    add image models to the models.py file

--- galleria/models.py
+++ galleria/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField()
+    image_name = models.CharField(max_length=30)
+    image_description = models.CharField(max_length=30)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Location, on_delete=models.CASCADE)
