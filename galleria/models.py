db485f7d9d9ad3077fd9d5934abd5653090b117c
zizoubrown <zizumoha2015@gmail.com>
2020-10-12 01:57:14 +0300

    debug

--- galleria/models.py
+++ galleria/models.py
@@ -18,7 +18,7 @@ class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_description = models.CharField(max_length=30)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    category = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.image_name
