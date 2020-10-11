9bad94512c4b2eaecb578d29e4a9b01e65532874
zizoubrown <zizumoha2015@gmail.com>
2020-10-12 01:52:53 +0300

    modify models.py to correct errors

--- galleria/models.py
+++ galleria/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    image = models.ImageField()
-    image_name = models.CharField(max_length=30)
-    image_description = models.CharField(max_length=30)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    category = models.ForeignKey(Location, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.image_name
-
 class Location(models.Model):
     location = models.CharField(max_length=30)
 
@@ -22,3 +12,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+
+class Image(models.Model):
+    #image = models.ImageField()
+    image_name = models.CharField(max_length=30)
+    image_description = models.CharField(max_length=30)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image_name
