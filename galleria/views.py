1bc3d63d798e306403e571b92b6d1134e9430742
zizoubrown <zizumoha2015@gmail.com>
2020-10-12 17:48:17 +0300

    modify home view function

--- galleria/views.py
+++ galleria/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Image,Location,Category
 
 # Create your views here.
-def image(request):
+def home(request):
     images = Image.objects.all()
     return render(request, 'home.html', {'images':images})
 
