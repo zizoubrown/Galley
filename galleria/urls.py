327938ecd03ff86abd39428ee3c01127284127f8
zizoubrown <zizumoha2015@gmail.com>
2020-10-12 21:16:45 +0300

    debug

--- galleria/urls.py
+++ galleria/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
     path('search/',views.search_results, name='search_results')
 ]
 
