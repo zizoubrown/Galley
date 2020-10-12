8e72e7956e9cfa31723bb6200e0bb14f9bc7d483
zizoubrown <zizumoha2015@gmail.com>
2020-10-12 13:47:35 +0300

    add images through my admin and modify admin site header and title

--- galleria/admin.py
+++ galleria/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Image,Location,Category
 
+admin.site.site_header = "Gallery Admin"
+admin.site.site_title = "Gallery Admin Area"
+admin.site.index_title = "Welcome to gallery admin"
+
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Category)
