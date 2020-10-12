d2a078603ebe4ca8b1bc22b91c4963c4aa13e824
zizoubrown <zizumoha2015@gmail.com>
2020-10-12 15:53:52 +0300

    add tmplate body that is going to render the images as well as configurations in the urls.py

--- galleria/urls.py
+++ galleria/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .import views
 
@@ -5,3 +7,5 @@ urlpatterns=[
     path('',views.home,name='home')
 ]
 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
